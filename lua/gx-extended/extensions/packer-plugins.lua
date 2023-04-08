530c39b22004150238472208afbc79356a723836
Ronnie Magatti <ronniemagatti@gmail.com>
2023-04-08 00:08:21 -0600

    Use the right url

--- lua/gx-extended/extensions/packer-plugins.lua
+++ lua/gx-extended/extensions/packer-plugins.lua
@@ -9,7 +9,7 @@ function M.setup(config)
 		match_to_url = function(line_string)
 			local line = string.match(line_string, '".*/.*"')
 			local repo = vim.split(line, ":")[1]:gsub('"', "")
-			local url = "https://www.npmjs.com/package/" .. repo
+			local url = "https://github.com/" .. repo
 
 			return url
 		end,
