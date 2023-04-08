3e5e8164bc098b6fe03013ce52067a151abd2851
Ronnie Magatti <ronniemagatti@gmail.com>
2023-04-08 00:05:29 -0600

    Make packer plugins extension use the new register

--- lua/gx-extended/extensions/packer-plugins.lua
+++ lua/gx-extended/extensions/packer-plugins.lua
@@ -4,11 +4,13 @@ function M.setup(config)
 	local lib = require("gx-extended.lib")
 	lib.setup(config)
 
-	lib.register_legacy({
+	lib.register({
 		autocmd_pattern = { "plugins.lua" },
-		pattern_to_match = ".*/.*",
-		match_to_url = function(yanked_string)
-			return "https://github.com/" .. yanked_string
+		match_to_url = function(line_string)
+			local match = string.match(line_string, '.*/.*')
+			local url = "https://github.com/" .. match
+
+			return url
 		end,
 	})
 end
