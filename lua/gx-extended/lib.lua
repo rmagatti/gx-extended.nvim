af2e24af5451f7e1a75e669854f30666eeaef91d
Ronnie Magatti <ronniemagatti@gmail.com>
2023-04-24 19:38:12 -0600

    yolo

--- lua/gx-extended/lib.lua
+++ lua/gx-extended/lib.lua
@@ -8,54 +8,55 @@ local M = {}
 ---			match_to_url = function() ... end,
 ---	} },
 ---}
-local per_pattern = {}
+local registry = {}
 
-local group = vim.api.nvim_create_augroup("gx-extended", {
-  clear = true,
-})
+-- local group = vim.api.nvim_create_augroup("gx-extended", {
+--   clear = true,
+-- })
+
+local function run_match_to_urls()
+  local line_string = vim.api.nvim_get_current_line()
+  local success, url = nil, nil
+
+  local filetype = vim.o.filetype
+
+  for _, value in ipairs(registry[filetype]) do
+    logger.debug({
+      per_pattern = registry[filetype],
+    })
+
+    success, url = pcall(value.match_to_url, line_string)
+
+    if url then
+      -- vim.api.nvim_call_function("netrw#BrowseX", { url, 0 })
+
+      logger.info({
+        line_string = line_string,
+        success = success,
+        url = url,
+      })
+      break
+    end
+  end
+
+  if not success then
+    logger.info("Could not match custom gx-extended pattern, calling default gx")
+
+    vim.cmd([[execute "normal \<Plug>NetrwBrowseX"]])
+    return
+  end
+end
 
 function M.setup(config)
   logger.log_level = config.log_level
+  vim.keymap.set("n", "gx", run_match_to_urls, {})
 
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*",
-    group = group,
-    callback = function()
-      vim.keymap.set("n", "gx", function()
-        local line_string = vim.api.nvim_get_current_line()
-        local success, url = nil, nil
-
-        for _, ptrn in ipairs(autocmd_pattern) do
-          for _, value in ipairs(per_pattern[ptrn]) do
-            logger.debug({
-              autocmd_pattern = autocmd_pattern,
-              per_pattern = per_pattern[ptrn],
-            })
-
-            success, url = pcall(value.match_to_url, line_string)
-
-            if url then
-              -- vim.api.nvim_call_function("netrw#BrowseX", { url, 0 })
-
-              logger.info({
-                line_string = line_string,
-                success = success,
-                url = url,
-              })
-              break
-            end
-          end
-        end
-
-        if not success then
-          logger.info("Could not match custom gx-extended pattern, calling default gx")
-
-          vim.cmd([[execute "normal \<Plug>NetrwBrowseX"]])
-          return
-        end
-      end, {})
-    end,
-  })
+  -- vim.api.nvim_create_autocmd("FileType", {
+  --   pattern = "*",
+  --   group = group,
+  --   callback = function()
+  --   end,
+  -- })
 end
 
 function M.register(options)
@@ -67,16 +68,17 @@ function M.register(options)
     events = events,
     autocmd_pattern = autocmd_pattern,
     match_to_url = match_to_url,
-    per_pattern = per_pattern,
+    per_pattern = registry,
   })
 
   -- Line up match_to_url functions for the same pattern so they are all executed when the autocmd is triggered
+  -- TODO: switch this to use filetypes instead of autocmd_pattern?
   for _, value in pairs(autocmd_pattern) do
-    if not per_pattern[value] then
-      per_pattern[value] = {}
+    if not registry[value] then
+      registry[value] = {}
     end
 
-    table.insert(per_pattern[value], { match_to_url = match_to_url })
+    table.insert(registry[value], { match_to_url = match_to_url })
   end
 end
 
